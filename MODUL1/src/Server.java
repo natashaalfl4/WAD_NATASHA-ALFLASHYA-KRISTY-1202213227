d0252825009f762cfed6b86d659a1f64a5f5c7c4
natashaalfl4 <natashaalflasya@gmail.com>
2022-11-05 11:09:46 +0700

    PRAKTIKUM MODUL 1

---
+++ MODUL1/src/Server.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class Server {
+    Scanner sc = new Scanner(System.in);
+    String nama = sc.nextLine();
+    public static void main(String[] args) {
+        // TODO Create Database
+
+        // TODO Create Menu
+
+        // TODO Insert Menu to Database
+
+
+        // TODO Display Main Menu
+
+        // TODO Create User
+
+        // Display Menu
+    
+    System.out.println ("Selamat Datang Di Restoran EAD");
+    System.out.println ("Silahkan Register Terlebih Dahulu");
+    System.out.println ("=================================");
+
+
+
+    }
+}
